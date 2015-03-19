0c16025a483039f9e68307141dfe263b71d113ff
Don Hsieh <don@kindminds.com>
2015-03-19 21:21:22 +0800

    Add schema/label.sql

---
+++ schema/label.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- 主機: localhost
+-- 建立日期: 2015 年 03 月 17 日 01:07
+-- 伺服器版本: 5.5.41-0ubuntu0.14.04.1
+-- PHP 版本: 5.5.9-1ubuntu4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 資料庫: `intelllex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `label`
+--
+
+CREATE TABLE IF NOT EXISTS `label` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(500) DEFAULT NULL,
+  `annotated` tinyint(1) DEFAULT NULL,
+  `is_case` tinyint(1) DEFAULT NULL,
+  `content` longtext,
+  `content_len` int(7) DEFAULT NULL,
+  `title` text,
+  `jurisdiction` char(8),
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `url` (`url`(255)),
+  KEY `annotated` (`annotated`),
+  KEY `is_case` (`is_case`),
+  KEY `content_len` (`content_len`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
