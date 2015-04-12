35ee9bab89e7850a69e923cb3fcdd42feea4f8bf
Don Hsieh <don@kindminds.com>
2015-04-12 18:48:16 +0800

    Add public static function addUser($post, $app) in php/app/models/User.php

--- php/app/models/User.php
+++ php/app/models/User.php
@@ -194,4 +194,121 @@ class User extends \Phalcon\Mvc\Model
 		);
 	}
 
+
+	/**
+	 * @function name:	public static function addStudent($post, $app)
+	 * @description:	This function creates an user account by content of $post.
+	 * @related issues: ITL-003
+	 * @param:			object $post
+	 * @param:			object $app
+	 * @return:			JSON $json
+	 * @author:			Don Hsieh
+	 * @since:			03/31/2015
+	 * @last modified:	03/31/2015
+	 * @called by:		$app->post('/api/student')
+	 *					 in php/public/index.php
+	 */
+	public static function addUser($post, $app)
+	{
+		$messages = array();
+		if (!$post) $messages['error'] = 'Please enter your username.';
+		
+		$user = array();
+		if (isset($post->username)) {
+			$user['username'] = $post->username;
+			$user['account'] = strtolower($post->username);
+		} else $messages['error'] = 'Please enter your institution.';
+		if (isset($post->type)) $user['type'] = $post->type;
+		else $messages['error'] = 'Please select your user type.';
+		if (isset($post->firstName)) $user['firstName'] = $post->firstName;
+		else $messages['firstName'] = 'Please enter your first name.';
+		if (isset($post->lastName)) $user['lastName'] = $post->lastName;
+		else $messages['lastName'] = 'Please enter your last name.';
+		if (isset($post->email)) $user['email'] = $post->email;
+		else $messages['email'] = 'Please enter your email.';
+
+			// 'ip' => 'ip', 
+			// 'ips' => 'ips', 
+		//password shall match
+		
+
+
+		if (isset($post->company)) $user['company'] = $post->company;
+		if (isset($post->monthFrom)) $user['monthFrom'] = $post->monthFrom;
+		if (isset($post->yearFrom)) $user['yearFrom'] = $post->yearFrom;
+		if (isset($post->monthFrom)) $user['monthFrom'] = $post->monthFrom;
+		if (isset($post->monthTo)) $user['monthTo'] = $post->monthTo;
+		if (isset($post->yearTo)) $user['yearTo'] = $post->yearTo;
+		if (isset($post->supervisor)) $user['supervisor'] = $post->supervisor;
+		if (isset($post->competitionName)) $user['competitionName'] = $post->competitionName;
+		if (isset($post->competitionResult)) $user['competitionResult'] = $post->competitionResult;
+		if (isset($post->publicationName)) $user['publicationName'] = $post->publicationName;
+		if (isset($post->publicationType)) $user['publicationType'] = $post->publicationType;
+		if (isset($post->publicationUrl)) $user['publicationUrl'] = $post->publicationUrl;
+		if (isset($post->publicationCitation)) $user['publicationCitation'] = $post->publicationCitation;
+		if (isset($post->qualification)) $user['qualification'] = $post->qualification;
+		if (isset($post->qualificationYear)) $user['qualificationYear'] = $post->qualificationYear;
+
+		if (isset($messages['error'])) {
+			$arr = array(
+				'status' => 'INVALID-INPUT',
+				'messages' => $messages,
+				'data' => $post
+			);
+			return $arr;
+		}
+
+		// $now = User::getNow();
+		$now = Practictioner::getNow();
+		$user['createdAt'] = $now;
+		$user['updatedAt'] = $now;
+		// Getting a request instance
+		// $request = new \Phalcon\Http\Request();
+		// $ipAddress = $request->getClientAddress();
+		// $ip = $ipAddress . ';' . $now;
+		// $arrIp = json_decode($row->ip);
+		// if (is_array($arrIp)) array_unshift($arrIp, $ip);
+		// else $arrIp = [$ip];
+		// $jsonIp = json_encode($arrIp);
+		// $user['ip'] = $jsonIp;
+
+		// $user['agent'] = $request->getUserAgent();
+		// $user['lang'] = $request->getBestLanguage();
+		$row = new Student();
+		// $result = $row->update($user, array(
+		$result = $row->save($user, array(
+			'institution', 'graduationYear', 'degree', 'company', 'monthFrom', 'yearFrom', 'monthTo',
+			'yearTo', 'supervisor', 'competitionName', 'competitionResult',
+			'publicationName', 'publicationType', 'publicationUrl', 'publicationCitation',
+			'qualification', 'qualificationYear', 'createdAt', 'updatedAt'
+		));;
+		
+		if (!$result) {
+			$arr = array();
+			foreach ($row->getMessages() as $message) {
+				$arr[] = $message;
+			}
+			$messages['error'] = implode('&nbsp;&nbsp;&nbsp;', $arr);
+			$arr = array(
+				'status' => 'SAVE-FAILED',
+				'messages' => $messages,
+				'editContent' => $row,
+				'result' => $result,
+				'data' => $post
+			);
+			return $arr;
+		} else {
+			$messages['success'] = 'Your profile has been updated.';
+			$arr = array(
+				'status' => 'EDIT-DONE',
+				'messages' => $messages,
+				'editContent' => $row,
+				'user' => $user,
+				'result' => $result,
+				'data' => $post
+			);
+			return $arr;
+		}
+	}
+
 }
